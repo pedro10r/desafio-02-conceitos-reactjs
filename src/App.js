e79b3a529c0453bae6be71c086432521d52eb0cf
PedroSantosRocha <pedrosantos.ti.web@gmail.com>
2020-04-21 13:34:14 -0300

    Fineshed challenge-02

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepository ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data);
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Projeto desafio',
+      owner: "Pedro Rocha"
+    });
+
+    const repository = response.data;
+
+    setRepository([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    
+    const projectIndex = repositories.findIndex(repository => repository.id === id);
+
+    if (projectIndex != -1) {
+      repositories.splice(projectIndex, 1);
+
+      setRepository([ ...repositories ]);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+        {repositories.map(repository =>
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
